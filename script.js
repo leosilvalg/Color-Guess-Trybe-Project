940ee86ac302b49fc03be9bde2bbee3cd0572356
Leonardo Gonçalves <leonardorc.geo@gmail.com>
2021-12-23 17:39:18 -0300

    Adicionando os arquivos

---
+++ script.js
@@ -0,0 +1,62 @@
+const cor = document.querySelector('#rgb-color'); 
+const corFundo = document.querySelectorAll('.ball'); 
+const texto = document.getElementById('answer'); 
+const reset = document.querySelector('#reset-game'); 
+const pontuacao = document.querySelector('#score'); 
+let score = 0;
+const easy = document.querySelector('#easy');
+const hard = document.querySelector('#hard');
+const numero = 6
+
+
+function cores() {
+  let corAleatoria = '';
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  corAleatoria = `RGB(${r}, ${g}, ${b})`;
+  return corAleatoria;
+}
+
+
+function pintaFundo() {
+  for (let i = 0; i < corFundo.length; i += 1) {
+    corFundo[i].style.backgroundColor = cores();
+  }
+  const valorObjeto = Math.floor(Math.random() * numero);
+  cor.innerText = corFundo[valorObjeto].style.backgroundColor;
+}
+pintaFundo();
+
+
+function pontos() {
+  score += 3;
+  pontuacao.innerText = `Score: ${score}`;
+}
+
+function escolha() {
+  texto.innerHTML = 'Choose a color!';
+}
+window.onload = escolha;
+
+function answer(event) {
+  const evT = event.target;
+  const corDeFundo = cor.innerText;
+  if (evT.style.backgroundColor.includes(corDeFundo)) {
+    texto.innerText = 'YEEAHH! You got it!!';
+    pontos();
+  } else {
+    texto.innerText = 'Sorry :( Try again!';
+  }
+}
+
+for (let index = 0; index < corFundo.length; index += 1) {
+  corFundo[index].addEventListener('click', answer);
+}
+
+function resetar() {
+  texto.innerHTML = 'Choose a color!';
+  pintaFundo();
+}
+
+reset.addEventListener('click', resetar);
